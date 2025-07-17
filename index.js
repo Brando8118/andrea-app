6d24bf31d9bf504060e757523e5a3cf214b0209d
Brando8118 <andreamariani10@libero.it>
2025-07-18 01:12:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+import '../styles/globals.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Benvenuto nell'area riservata: studio</h1>
+      <p>Caricamento documenti, appuntamenti e videochiamate disponibili nella versione completa.</p>
+    </div>
+  );
+}
